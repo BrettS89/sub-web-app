3b2c06d5f1e55362c94a5320c2c2112259211a88
BrettS89 <blsodie@gmail.com>
2020-04-22 17:16:23 -0400

    prep for prod

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,14 @@
 // LOCAL
-export const URI = 'http://localhost:4000/api';
+// export const URI = 'http://localhost:4000/api';
 
 // STAGING
 // export const URI = 'https://paradyse-staging.herokuapp.com/api';
 
 // PROD
-// export const URI = 'https://paradyse-prod.herokuapp.com/api'
+export const URI = 'https://paradyse-prod.herokuapp.com/api'
 
 // Stripe staging client ID
 export const strpieClientId = 'ca_H1GujGahdbIJfrmLlFCzgURlQMnZW1Ld';
+
+// Stripe prod client id
+// tbd
