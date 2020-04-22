f90403e7ecd2efe8c48d3ad40558b42980a3c1c0
BrettS89 <blsodie@gmail.com>
2020-04-22 17:16:12 -0400

    prep for prod

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ export const URI = 'http://localhost:4000/api';
 // export const URI = 'https://paradyse-staging.herokuapp.com/api';
 
 // PROD
-//TBD
+// export const URI = 'https://paradyse-prod.herokuapp.com/api'
 
 // Stripe staging client ID
 export const strpieClientId = 'ca_H1GujGahdbIJfrmLlFCzgURlQMnZW1Ld';
