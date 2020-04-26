32b71b4caf6bb8c527d4df5bdfbf6de43929dadb
BrettS89 <blsodie@gmail.com>
2020-04-26 19:32:53 -0400

    adding cancelled subscription text

--- src/components/UseCredits/Components/Subscription.js
+++ src/components/UseCredits/Components/Subscription.js
@@ -16,10 +16,21 @@ const Subscription = ({ subscription, openModal }) => {
     });
   }
 
+  function renderCancelledBySpot() {
+    if (subscription.cancelledBySpot) {
+      return (
+        <span className="UseCredits-cancelled">
+          This subscription has been cancelled by the local business and will be removed once all credits are used.
+        </span>
+      )
+    }
+  }
+
   return (
     <div className="UseCredits-subscription">
       <h2>{subscription.name}</h2>
       {renderItemCredits()}
+      {renderCancelledBySpot()}
     </div>
   );
 };
