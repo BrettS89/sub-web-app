1efb98412016c3b3415378e4f4519742d7821940
BrettS89 <blsodie@gmail.com>
2020-05-09 12:55:30 -0400

    adding margin to logout

--- src/config/index.js
+++ src/config/index.js
@@ -2,15 +2,15 @@
 // export const URI = 'http://localhost:4000/api';
 
 // STAGING
-// export const URI = 'https://paradyse-staging.herokuapp.com/api';
+export const URI = 'https://paradyse-staging.herokuapp.com/api';
 
 // PROD
-export const URI = 'https://paradyse-prod.herokuapp.com/api'
+// export const URI = 'https://paradyse-prod.herokuapp.com/api'
 
 // Stripe staging client ID
 
-// export const stripeClientId = 'ca_H1GujGahdbIJfrmLlFCzgURlQMnZW1Ld';
+export const stripeClientId = 'ca_H1GujGahdbIJfrmLlFCzgURlQMnZW1Ld';
 export const stripeState = 'stonecold';
 
 // Stripe prod client id
-export const stripeClientId = 'ca_H1GuCuTXObeA9iU0UFyzp9AoLqQyBuwK';
+// export const stripeClientId = 'ca_H1GuCuTXObeA9iU0UFyzp9AoLqQyBuwK';
